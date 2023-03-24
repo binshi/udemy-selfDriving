381f30ef8d9d3092a730b51f353258e0a983497f
Shibin Menachery <menacher@C02FW3K6MD6N.ad.here.com>
2023-03-24 15:28:49 +0530

    availability zones fixclear

--- cd11573-multicloud-computing-main/lesson-5-deploying-multi-cloud-services/exercise-2/solution/aws/main.tf
+++ cd11573-multicloud-computing-main/lesson-5-deploying-multi-cloud-services/exercise-2/solution/aws/main.tf
@@ -18,7 +18,7 @@ data "aws_availability_zones" "available_zones" {
 resource "aws_subnet" "public" {
   count                   = 2
   cidr_block              = cidrsubnet(aws_vpc.default.cidr_block, 8, 2 + count.index)
-  availability_zone       = data.aws_availability_zones.available_zones.names[count.index]
+  # availability_zone       = data.aws_availability_zones.available_zones.names[count.index]
   vpc_id                  = aws_vpc.default.id
   map_public_ip_on_launch = true
 }
@@ -26,9 +26,9 @@ resource "aws_subnet" "public" {
 resource "aws_subnet" "private" {
   count             = 2
   cidr_block        = cidrsubnet(aws_vpc.default.cidr_block, 8, count.index)
-  availability_zone = data.aws_availability_zones.available_zones.names[count.index]
+  # availability_zone = data.aws_availability_zones.available_zones.names[count.index]
   vpc_id            = aws_vpc.default.id
-}
+} 
 
 resource "aws_internet_gateway" "gateway" {
   vpc_id = aws_vpc.default.id
