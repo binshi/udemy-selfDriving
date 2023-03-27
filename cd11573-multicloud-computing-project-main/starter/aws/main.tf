71e7a8cd08e5c38f1d9427c3047d3688e7946897
Shibin Menachery <menacher@C02FW3K6MD6N.ad.here.com>
2023-03-27 16:10:42 +0530

    name update

--- cd11573-multicloud-computing-project-main/starter/aws/main.tf
+++ cd11573-multicloud-computing-project-main/starter/aws/main.tf
@@ -179,11 +179,11 @@ resource "aws_ecs_task_definition" "udacity_app" {
     "environment": [
       {
         "name": "AZURE_SQL_SERVER",
-        "value": "udacity-tscotto-azure-sql"
+        "value": "udacity-shibin-azure-sql"
       },
       {
         "name": "AZURE_DOTNET_APP",
-        "value": "udacity-tscotto-azure-dotnet-app"
+        "value": "udacity-shibin-azure-dotnet-app"
       }
     ],
     "portMappings": [
